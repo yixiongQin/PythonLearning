1496f02a26a0b9690974160f145a9fa9b6829b80
yixiongQin <jianadamengtelier@outlook.com>
2024-04-30 11:09:24 -0400

    while loop

---
+++ 01-FirstCourse/pythonProject/for_loop.py
@@ -0,0 +1,20 @@
+for item in [1,2,3,4,5]:
+    print(item)
+
+# output
+# 1
+# 2
+# 3
+# 4
+# 5
+
+
+for item in range(10):
+    print(item)
+
+for item in range(5,10):
+    print(item)
+
+for item in range(5,10,2):
+    print(item)
+
