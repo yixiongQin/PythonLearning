a10040d7a7a45bb85c28dce4abb2d1396fdd6cea
lisiCAO <caolisi@hotmail.com>
2024-05-08 23:29:24 -0400

    update

---
+++ 01-FirstCourse/pythonProject/emoji_convert.py
@@ -0,0 +1,12 @@
+message = input(">")
+words = message.split(' ')
+# print(words)
+emojis = {
+    ":)" : "😄",
+    ":(" : "😫"
+}
+output = ""
+for word in words:
+    output += emojis.get(word, word) + " "
+
+print(output)
